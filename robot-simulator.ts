f83d3d8da27115b7bc3b3cfa2bcf0628a9720ade
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-30 12:24:32 +0200

    Robot hecho

--- robot-simulator.ts
+++ robot-simulator.ts
@@ -1,27 +1,67 @@
+import { stringify } from "querystring";
+
 export class InvalidInputError extends Error {
   constructor(message: string) {
     super()
     this.message = message || 'Invalid Input'
   }
 }
-
+const directions: string[] = ['north', 'east', 'south', 'west']
 type Direction = 'north' | 'east' | 'south' | 'west'
 type Coordinates = [number, number]
-
 export class Robot {
+  direction: Direction = 'north';
+  coords: Coordinates = [0, 0];
   get bearing(): Direction {
-    throw new Error('Remove this statement and implement this function')
+    return this.direction;
   }
-
   get coordinates(): Coordinates {
-    throw new Error('Remove this statement and implement this function')
+    return this.coords;
   }
-
-  place({}: { x: number; y: number; direction: string }) {
-    throw new Error('Remove this statement and implement this function')
+  place(robot: { x: number; y: number; direction: string }) {
+    if (robot.direction != 'north' && robot.direction != 'east' && robot.direction != 'south' && robot.direction != 'west') {
+      throw new InvalidInputError("No valido");;
+    }
+    this.direction = robot.direction as Direction;
+    this.coords = [robot.x, robot.y];
   }
-
   evaluate(instructions: string) {
-    throw new Error('Remove this statement and implement this function')
+    let i: number = 0;
+    let letra: string;
+    for (i; i < instructions.length; i++) {
+      letra = instructions[i];
+      switch (letra) {
+        case 'R':
+          if (this.direction == 'west') {
+            this.direction = directions[0] as Direction;
+          } else {
+            this.direction = directions[directions.indexOf(this.direction) + 1] as Direction;
+          }
+          break;
+        case 'L':
+          if (this.direction == 'north') {
+            this.direction = directions[3] as Direction;
+          } else {
+            this.direction = directions[directions.indexOf(this.direction) - 1] as Direction;
+          }
+          break;
+        case 'A':
+          switch (this.direction) {
+            case 'north':
+              this.coords[1] +=1
+              break;
+            case 'east':
+              this.coords[0]+=1
+              break;
+            case 'south':
+              this.coords[1] -= 1;
+              break;
+            case 'west':
+              this.coords[0] -= 1
+              break;
+          }
+          break;
+      }
+    }
   }
 }
